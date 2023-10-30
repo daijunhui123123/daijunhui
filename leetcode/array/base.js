e033bbcced2d9f7e562c26a5174010ffd3398355
daijunhui123123 <204283409@qq.com>
2023-10-30 20:16:21 +0800

    算法

---
+++ leetcode/array/base.js
@@ -0,0 +1,21 @@
+// var str = 'hello  world'  //string 字符串
+// var num = 123 //number 数字
+// var flag = false // boolean 布尔值
+// var un = undefined // 找不到的一个值
+// var n = null   // 没有值 空的
+
+// // 应用类型  对象  万物皆对象
+// var obj = {
+//     name: 戴俊辉,
+//     age: 18,
+// };  // 就是键值对
+// // console.log(obj.name)
+// var arr = ['a', 'b', 1]
+// arr[0] = 1
+// arr[5] = true;
+// arr.push(5)//在数组最后加一个数字
+// arr.pop(5)//在数组最后删除一个数组
+// arr.unshift(5)  ////在数组最前面加一个数字
+// arr.shift(5)//  在数组最前删除一个数组
+// arr.splice(a, b, c)  //a表示删除数组的下标   b表示要删调几个
+// arr.splice(1,0,0)  //表示在下标一后面添加一个0
