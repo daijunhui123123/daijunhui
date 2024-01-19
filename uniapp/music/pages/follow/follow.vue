b8c4989d4da9a0a26c7d645901c39fe25fcafcad
daijunhui123123 <204283409@qq.com>
2024-01-19 22:19:05 +0800

    网易云一

---
+++ uniapp/music/pages/follow/follow.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="follow">
+	     <wyheader icon="plus-filled">
+			 <template v-slot:content>
+			 		 <text>动态</text>
+			 </template>
+		
+		 </wyheader>
+	</view>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
